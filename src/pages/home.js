88b5de73dcac732aeacccf893b720d174efe25ab
param patel <parampatel323@gmail.com>
2024-03-08 09:00:57 +0530

    first commit

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,6 +3,8 @@ export default function Home () {
         <>
         <Homebanner />
         <Homewelcome />
+        <Homedesc />
+        <Homeslides />
         </>
     );
 }
@@ -122,4 +124,84 @@ function Homewelcome () {
     );
 }
 
-export { Homebanner, Homewelcome }
+function Homedesc () {
+    return (
+        <>
+        <section class="bottom-banner-w3layouts">
+            <div class="d-lg-flex">
+                <div class="col-lg-7 p-lg-0 text-lg-right text-center mt-lg-0 mt-4 bottom-left">
+                </div>
+                <div class="col-lg-5 banner-about text-center">
+                    <span class="fa fa-scissors"></span>
+                        <h4 class="mt-sm-4 mt-2">making hair style</h4>
+                        <h5 class="bottom mt-m-4 mt-3">For man growing beards!</h5>
+                        <p class="">Sed ut perspiciatis unde omnis iste natus error ipsum voluptatem ut accusa ntium dolor remque laudantium, totam rem
+                        aperiam, eaque ipsa quae abse illo quasi sed architecto beatae vitae dicta sut dolor etr explicabo. Morbi a luctus magna, eu rutrum
+                        turpis. Sed perspiciatis unde.</p>
+                </div>
+            </div>
+        </section>
+        </>
+    );
+}
+
+function Homeslides() {
+    return (
+        <>
+        <section class="services py-5" id="services">
+            <div class="container py-lg-5 py-3">
+                <div class="row service-grid-grids text-center">
+                    <div class="col-lg-4 col-md-6 service-grid service-grid1">
+                        <div class="service-icon">
+                            <span class="fa fa-puzzle-piece"></span>
+                        </div>
+                        <h4 class="mt-3">Skilled Barbers</h4>
+                        <p class="mt-3">Perspiciatis unde omnis iste natus doloret ipsum volupte ut accusal ntium dolor remque laudantium, totam dolor.</p>
+                    </div>
+                    <div class="col-lg-4 col-md-6 service-grid service-grid2 mt-md-0 mt-5">
+                        <div class="service-icon">
+                            <span class="fa fa-scissors"></span>
+                        </div>
+                        <h4 class="mt-3">Hair stylists</h4>
+                        <p class="mt-3">Perspiciatis unde omnis iste natus doloret ipsum volupte ut accusal ntium dolor remque laudantium, totam dolor.</p>
+                    </div>
+                    
+                    <div class="col-lg-4 col-md-6 service-grid service-grid3 mt-lg-0 mt-5">
+                        <div class="service-icon">
+                            <span class="fa fa-sliders"></span>
+                        </div>
+                        <h4 class="mt-3">Beard Grooming</h4>
+                        <p class="mt-3">Perspiciatis unde omnis iste natus doloret ipsum volupte ut accusal ntium dolor remque laudantium, totam dolor.</p>
+                    </div>
+                </div>
+                <div class="row mt-5">
+                    <div class="col-md-6 p-md-0">
+                        <div class="bg-image-left">	
+                            <h4>skilled barbers</h4>
+                        </div>
+                    </div>
+                    <div class="col-md-6 p-md-0">
+                        <div class="bg-image-right">
+                            <h4>skilled barbers</h4>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6 pr-md-0">
+                                <div class="bg-image-bottom1">
+                                    <h4>Trimming</h4>
+                                </div>
+                            </div>
+                            <div class="col-md-6 pl-md-0">
+                                <div class="bg-image-bottom2">
+                                    <h4>Shaving</h4>
+                                </div>
+                            </div>
+                        </div>	
+                    </div>	
+                </div>		
+            </div>		
+        </section>
+        </>
+    );
+}
+
+export { Homebanner, Homewelcome, Homedesc, Homeslides }
